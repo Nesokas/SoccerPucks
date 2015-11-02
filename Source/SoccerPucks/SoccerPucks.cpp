56f28d4b262bc25625a14363213238b8be65825f
Nuno Costa <nuno.pinto.costa@gmail.com>
2015-11-02 12:37:01 +0900

    1st Commit part 2

---
+++ Source/SoccerPucks/SoccerPucks.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SoccerPucks.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SoccerPucks, "SoccerPucks" );
---
+++ Source/SoccerPucks/SoccerPucksGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SoccerPucksGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOCCERPUCKS_API ASoccerPucksGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
