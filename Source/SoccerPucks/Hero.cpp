872c50f3d08afd5e0a0c4c94c539d2f74d43d937
Ines Moital <ines.moital@gmail.com>
2015-11-14 17:36:40 +0900

    Start to implement movement

---
+++ Source/SoccerPucks/Hero.cpp
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SoccerPucks.h"
+#include "Hero.h"
+
+// Sets default values
+AHero::AHero()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set this pawn to be controlled by the lowest-numbered player
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	// Create a dummy root component we can attach things to.
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	//UCameraComponent* OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
+	OurVisibleComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("OurVisibleComponent"));
+
+	/*OurCamera->AttachTo(RootComponent);
+	OurCamera->SetRelativeLocation(FVector(-250.0f, 0.0f, 250.0f));
+	OurCamera->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));*/
+	OurVisibleComponent->AttachTo(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AHero::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHero::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+
+
+	// Handle movement based on our "MoveX" and "MoveY" axes
+	{
+		if (!CurrentVelocity.IsZero())
+		{
+			FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+			SetActorLocation(NewLocation);
+		}
+	}
+
+}
+
+// Called to bind functionality to input
+void AHero::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+	// Respond when our "Grow" key is pressed or released.
+	InputComponent->BindAction("Grow", IE_Pressed, this, &AHero::StartGrowing);
+	InputComponent->BindAction("Grow", IE_Released, this, &AHero::StopGrowing);
+
+	// Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
+	InputComponent->BindAxis("MoveX", this, &AHero::Move_XAxis);
+	InputComponent->BindAxis("MoveY", this, &AHero::Move_YAxis);
+
+}
+
+void AHero::Move_XAxis(float AxisValue)
+{
+	// Move at 100 units per second forward or backward
+	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
+
+void AHero::Move_YAxis(float AxisValue)
+{
+	// Move at 100 units per second right or left
+	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
+
+void AHero::StartGrowing()
+{
+	bGrowing = true;
+}
+
+void AHero::StopGrowing()
+{
+	bGrowing = false;
+}
